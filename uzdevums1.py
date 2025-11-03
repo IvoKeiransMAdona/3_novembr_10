323450438bca0aab69c96c89b72e7f800cf2d799
IvoKeiransMAdona <ivoedmundsk@gmail.com>
2025-11-03 10:57:16 +0200

    Izlabotas kļudasuzdevums1.py
    Atrastas tris kļudas

--- uzdevums1.py
+++ uzdevums1.py
@@ -1,8 +1,8 @@
 import math
 # Ievade
-a = float(input("Ievadi a: "))
-b = float(input("Ievadi b: "))
-c = float(input("Ievadi c: ")
+a = float(input("Ievadi a: 1"))
+b = float(input("Ievadi b: 1"))
+c = float(input("Ievadi c: 1")) #trukst iekavas
 
 # Diskriminants
 D = b**2 - 4*a*c
@@ -10,10 +10,10 @@ D = b**2 - 4*a*c
 # Sakņu aprēķins
 if D > 0:
     x1 = (-b + math.sqrt(D)) / (2*a)
-    x = (-b - math.sqrt(D)) / (2*a)
+    x2 = (-b - math.sqrt(D)) / (2*a) # nepareiza mainiga nosakums
     print(f"D = {D}, divas saknes: x1 = {x1}, x2 = {x2}")
 elif D == 0:
     x = -b / (2*a)
-    print(fD = {D}, viena sakne: x = {x}")
+    print(f"D = {D}, viena sakne: x = {x}")  #iztrukst pedinas
 else:
     print(f"D = {D}, nav reālu sakņu.")
